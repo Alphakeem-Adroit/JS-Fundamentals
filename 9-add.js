0506d524785a362b041cc2457f22c0508c8358e8
Alphakeem-Adroit <alphakeem@Alphakeem.localdomain>
2025-11-09 09:28:10 +0100

    Fix: argument value

--- 9-add.js
+++ 9-add.js
@@ -1,14 +1,12 @@
-const args = process.argv.length - 2;
+function add(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return "NaN";
+  } else {
+    return a + b;
+  }
+}
 
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-
-function add(a, b) {
-    if (isNaN(a) || isNaN(b)) {
-        console.log("NaN");
-    } else {
-        return a + b;
-    }
-}
 
 console.log(add(a, b));
