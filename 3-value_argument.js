13e7989f9e895dd9a27b7ec667a1bac32b9f8a04
Alphakeem-Adroit <alphakeem@Alphakeem.localdomain>
2025-11-09 09:14:28 +0100

    Fix: Length keyword removed

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,13 +1,3 @@
-// function arg(num1, num2) {
-//     if (arguments.length < 1) {
-//         console.log("No argument");
-//     } else {
-//         console.log(arguments[0]);
-//     }
-// }
-
-// arg();
-
 const args = process.argv - 2;
 
 if (args <1) {
